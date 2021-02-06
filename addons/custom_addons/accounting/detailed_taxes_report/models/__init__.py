0d5c1650784b6838486cc98f36c28d69e57bcbb3
Hager Hilal <Hagerhilal2@gmail.com>
2021-02-06 20:29:12 +0200

    detailed tax report

--- addons/custom_addons/accounting/detailed_taxes_report/models/__init__.py
+++ addons/custom_addons/accounting/detailed_taxes_report/models/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import partner_receipt
+from . import taxes
--- addons/custom_addons/accounting/detailed_taxes_report/report/__init__.py
+++ addons/custom_addons/accounting/detailed_taxes_report/report/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import partner_receipt_report
+from . import detailed_tax_report
