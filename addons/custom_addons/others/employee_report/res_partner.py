99fb57876ee3d583db2679a270a4347154be3823
Hager Hilal <Hagerhilal2@gmail.com>
2021-02-08 18:50:48 +0200

    vendors purchase addons

--- addons/custom_addons/others/employee_report/res_partner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
-
-from odoo import models, fields, api, _
-
-
-class hr_employee(models.Model):
-    _inherit = "hr.employee"
--- addons/custom_addons/others/partner_report/res_partner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
-
-from odoo import models, fields, api, _
-
-class res_partner(models.Model):
-    _inherit = "res.partner"
-
