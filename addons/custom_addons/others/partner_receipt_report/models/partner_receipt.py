2ce3c650b180efad58460a08f06e38823275df37
Hager Hilal <Hagerhilal2@gmail.com>
2021-02-12 22:14:48 +0200

    Receipt

--- addons/custom_addons/others/partner_receipt_report/models/partner_receipt.py
+++ addons/custom_addons/others/partner_receipt_report/models/partner_receipt.py
@@ -15,10 +15,14 @@ class PartnerReceipt(models.TransientModel):
     _name = 'partner.receipt'
     
     date = fields.Date(string='Date', required=True)
+    
+    from_account = fields.Many2one('account.account', string='From Account', required=True)
+    to_account = fields.Many2one('account.account', string='To Account', required=True)
 
     partner_id = fields.Many2one('res.partner', string='Partner', required=True, help='Select Partner for movement')
+    amount = fields.Float(string='Amount', default=0.0)
     
     @api.multi
     def print_report(self, data):
-        data = {'partner_id': self.partner_id ,'date': self.date }
+        data = {'partner_id': self.partner_id.id ,'date': self.date , 'amount' : self.amount , 'from' : self.from_account , 'to': self.to_account  }
         return self.env.ref('partner_receipt_report.partner_receipt').report_action(self, data=data)
---
+++ addons/custom_addons/others/partner_receipt_report/report/partner_receipt_report.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2015 Dynexcel (<http://dynexcel.com/>).
+#
+##############################################################################
+import pytz
+import time
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError, Warning
+
+
+class CustomReport(models.TransientModel):
+    _name = "report.partner_receipt_report.partner_receipt_pdf"
+
+    def _get_report_values(self,docids,data=None):       
+        return {
+            'doc_ids': self.ids,
+             docs : docids ,
+            'doc_model': 'partner.receipt',
+            'data': data,
+        }
