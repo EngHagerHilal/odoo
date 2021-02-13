ca0b6a904455dccf409927e2b7c2231f4f4bda4a
Hager Hilal <Hagerhilal2@gmail.com>
2021-02-13 13:36:57 +0200

    Receipt

--- addons/custom_addons/others/partner_receipt_report/report/partner_receipt_report.py
+++ addons/custom_addons/others/partner_receipt_report/report/partner_receipt_report.py
@@ -28,10 +28,10 @@ class CustomReport(models.TransientModel):
             'doc_model': 'partner.receipt',
             'data': data,
             'date' : data['date'],
-            'date' = data['date'],
-            'partner' = data['partner_id'],
-            'amount' = data['amount'],
-            'from' = data['from'],
-            'to' = data['to'],
+            'date' : data['date'],
+            'partner' : data['partner_id'],
+            'amount' : data['amount'],
+            'from' : data['from'],
+            'to' : data['to'],
             'dat' : data
         }
