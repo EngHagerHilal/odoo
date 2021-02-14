1f3b78fa078410eb940f442789713d444e402cf3
Hager Hilal <Hagerhilal2@gmail.com>
2021-02-14 20:40:57 +0200

    Unique

--- addons/custom_addons/others/sale_purchase_unique_number/__manifest__.py
+++ addons/custom_addons/others/sale_purchase_unique_number/__manifest__.py
@@ -3,14 +3,11 @@
 {
     'name': 'sale_purchase_unique_number',
     'version': '12.0.1.0.0',
-    'category': 'Sales',
-    'license': 'AGPL-3',
     'author': 'Leen Company'
     'maintainer': 'Leen Company',
     'summary': 'Set unique number for sale order and purchase order',
     'depends': [
-        'sale',
-        'purchase'
+        'sale'
     ],
     'installable': True,
     'auto_install': False,
