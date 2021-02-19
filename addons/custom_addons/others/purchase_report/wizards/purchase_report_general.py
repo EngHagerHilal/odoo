f052c4187934caa23434b85a8cc26341c18e986e
Hager Hilal <Hagerhilal2@gmail.com>
2021-02-19 20:16:51 +0200

    purchase Report

--- addons/custom_addons/others/purchase_report/wizards/purchase_report_general.py
+++ addons/custom_addons/others/purchase_report/wizards/purchase_report_general.py
@@ -12,13 +12,19 @@ class PurchaseReportVendor(models.TransientModel):
     
 
     def print_purchase_report(self):
-        purchase_order = self.env['purchase.order'].search([('x_car_number','=',self.car_num),('x_driver','=',self.driver),('date_order','>=' ,self.start_date), ('date_order', '<=' , self.end_date)]).browse()
+        purchase_order = self.env['purchase.order'].search([('x_car_number','=',self.car_num),('x_driver','=',self.driver),('date_order','>=' ,self.start_date), ('date_order', '<=' , self.end_date)]).id
+        orders = [] 
+        for order in purchase_order :
+            new = self.env['purchase.order'].browse(order)
+            order.append(order)
+
+        
         filtered_purchase_order = list(filter(lambda x: ( x.x_car_number == self.car_num and (x.x_driver == self.driver or x.x_paid_driver == self.driver) and x.date_order >= self.start_date and x.date_order <= self.end_date)  , purchase_order))
 
         datas = {
             'ids': self,
             'model': 'purchase.report.general',
-            'out' : purchase_order ,
+            'out' : orders ,
             'form': filtered_purchase_order,
             'start_date': self.start_date,
             'end_date': self.end_date ,
