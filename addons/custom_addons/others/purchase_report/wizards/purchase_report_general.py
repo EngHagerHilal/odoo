bbb4f42e35d31d23f8573d0001afa42720e0e7c0
Hager Hilal <Hagerhilal2@gmail.com>
2021-02-19 22:33:21 +0200

    purchase Report

--- addons/custom_addons/others/purchase_report/wizards/purchase_report_general.py
+++ addons/custom_addons/others/purchase_report/wizards/purchase_report_general.py
@@ -12,14 +12,33 @@ class PurchaseReportVendor(models.TransientModel):
     
 
     def print_purchase_report(self):
-        purchase_order = self.env['purchase.order'].search([('x_car_number','=',self.car_num),('x_driver','=',self.driver),('date_order','>=' ,self.start_date), ('date_order', '<=' , self.end_date)])
+        #purchase_order = self.env['purchase.order'].search([('x_car_number','=',self.car_num),('x_driver','=',self.driver),('date_order','>=' ,self.start_date), ('date_order', '<=' , self.end_date)])
+        purchase_order = self.env['purchase.order'].search([])
+        filtered_purchase_order = list(filter(lambda x: ( x.x_car_number == self.car_num and (x.x_driver == self.driver or x.x_paid_driver == self.driver) and x.date_order >= self.start_date and x.date_order <= self.end_date)  , purchase_order))
+
         orders = [] 
         for order in purchase_order :
-            new = self.env['purchase.order'].browse(order)
-            orders.append(order)
-
+            temp_data = []
+            temp_data.append(order.name)
+            temp_data.append(order.date_order)
+            temp_data.append(order.x_balance)
+            temp_data.append(order.amount_total)
+            temp_data.append(order.partner_id)
+            temp_data.append(order.amount_tax)
+            temp_data.append(order.amount_untaxed)
+            temp_data.append(order.x_driver)
+            temp_data.append(order.x_car_number)
+            temp_data.append(order.x_paid_driver)
+            lines = []
+            for line in order.order_line :
+                line_details = []
+                line_details.append(line.product_id)
+                line_details.append(line.product_qty)
+                line_details.append(line.qty_received)
+                lines.append(line_details)
+            temp_data.append(lines)
+            orders.append(temp_data)
         
-        filtered_purchase_order = list(filter(lambda x: ( x.x_car_number == self.car_num and (x.x_driver == self.driver or x.x_paid_driver == self.driver) and x.date_order >= self.start_date and x.date_order <= self.end_date)  , purchase_order))
 
         datas = {
             'ids': self,
