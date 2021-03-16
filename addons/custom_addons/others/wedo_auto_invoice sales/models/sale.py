6bf9a9e53d71c066139025855cd5880ed825f2be
Hager Hilal <Hagerhilal2@gmail.com>
2021-03-16 13:22:03 +0200

    purchase Report

--- addons/custom_addons/others/wedo_auto_invoice sales/models/sale.py
+++ addons/custom_addons/others/wedo_auto_invoice sales/models/sale.py
@@ -23,7 +23,7 @@ class SaleOrder(models.Model):
             'sale_id': self.id,
             'origin': self.name,
         })
-        bill._default_comment()
+        bill.action_invoice_create()
         invoice_vals = bill._convert_to_write(bill._cache)
         #self.invoice_ids += bill
         self.env['account.invoice'].create(invoice_vals)
