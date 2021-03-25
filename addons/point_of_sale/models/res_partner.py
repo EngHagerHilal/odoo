f74cbabaf44e1215a767abb346f88af977775f40
Hager Hilal <Hagerhilal2@gmail.com>
2021-03-25 21:46:25 +0200

    change pos.xml

--- addons/point_of_sale/models/res_partner.py
+++ addons/point_of_sale/models/res_partner.py
@@ -11,6 +11,7 @@ class ResPartner(models.Model):
         help="The number of point of sales orders related to this customer",
         groups="point_of_sale.group_pos_user",
     )
+    
 
     def _compute_pos_order(self):
         partners_data = self.env['pos.order'].read_group([('partner_id', 'in', self.ids)], ['partner_id'], ['partner_id'])
