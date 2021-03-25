0e6d0700e30d4ddae21cfc7fad6daadd33035419
Hager Hilal <Hagerhilal2@gmail.com>
2021-03-25 20:52:15 +0200

    pos

---
+++ addons/partner_civil_id/__manifest__.py
@@ -0,0 +1,20 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Partner Civil ID',
+    'version': '12.0.1.0.0',
+    'category': 'Partner',
+    'license': 'AGPL-3',
+    'author': 'Leen',
+    'website': 'http://www.leen-eg.com',
+    'maintainer': 'Leen.',
+    'summary': 'Set civil id for every customer',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/partner_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
