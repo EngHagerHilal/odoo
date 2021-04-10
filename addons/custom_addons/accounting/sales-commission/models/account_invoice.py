c4a539ca593c2aa17e3edee1c6a3ae12b1c82135
Hager Hilal <Hagerhilal2@gmail.com>
2021-04-10 13:17:49 +0200

    emails

--- addons/custom_addons/accounting/sales-commission/models/account_invoice.py
+++ addons/custom_addons/accounting/sales-commission/models/account_invoice.py
@@ -12,21 +12,46 @@ class AccountInvoice(models.Model):
         compute="compute_commission",
         default=0,
     )
-    @api.onchange('state')
+    deadline = fields.Datetime(string="Deadline" , readOnly = True , compute="compute_deadline")
+    payment_date = fields.Date(string="payment date" , readOnly = True , compute="compute_payment_date")
+
+    
+    def deadline(self):
+        date = self.date_invoice
+        x2 = self.date_invoice
+        if date.day == 1 or date.day == 2 or date.day == 3 or date.day == 4 :
+            x2 = datetime.datetime(date.year, date.month , 5)
+        else :
+            if date.month != 12:
+                x2 = datetime.datetime(date.year, date.month+1 , 5)
+            else :
+                x2 = datetime.datetime(date.year+1, 1 , 5)
+        self.deadline = x2
+    
+    def compute_payment_date(self):
+        if (self.move_id):
+            self.payment_date = self.date_invoice
+            for move in self.move_id :
+                if  move.date > self.payment_date :
+                    self.payment_date = move.date
+                
+   
     def compute_commission(self) :
         if (self.state == 'paid'):
             if self.write_date >= self.x_sale_agent.last_reset :
-                if (self.write_date.date() - self.date_invoice).days <= 1 :
-                    if self.invoice_line_ids :
-                        count = 0
-                        diff = 0
-                        for line in self.invoice_line_ids :
-                            if line.product_id.categ_id.commission :
-                                count = count + line.quantity
-                            if line.product_id.public_price < line.price_unit and line.product_id.public_price != 0 :
-                                diff = line.quantity * line.price_unit - line.quantity * line.product_id.public_price 
-                            self.commission += count * line.product_id.categ_id.super_commission_rate + diff / 2
-                else :
+                if ( self.payment_date < self.deadline.date() ):
+
+                    if (self.payment_date - self.date_invoice).days <= 1 :
+                        if self.invoice_line_ids :
+                            count = 0
+                            diff = 0
+                            for line in self.invoice_line_ids :
+                                if line.product_id.categ_id.commission :
+                                    count = count + line.quantity
+                                if line.product_id.public_price < line.price_unit and line.product_id.public_price != 0 :
+                                    diff = line.quantity * line.price_unit - line.quantity * line.product_id.public_price 
+                                self.commission = count * line.product_id.categ_id.super_commission_rate + diff / 2
+                    else :
                         if self.invoice_line_ids :
                              count = 0
                              for line in self.invoice_line_ids :
@@ -34,5 +59,7 @@ class AccountInvoice(models.Model):
                                     count = count + line.quantity
                                  if line.product_id.public_price < line.price_unit and line.product_id.public_price != 0 :
                                     diff = line.quantity * line.price_unit - line.quantity * line.product.public_price 
-                                 self.commission += count * line.product_id.categ_id.default_commission_rate + diff / 2       
+                                 self.commission = count * line.product_id.categ_id.default_commission_rate + diff / 2
+        else :
+            self.commission = 0       
 
