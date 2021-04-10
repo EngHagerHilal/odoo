8ee3498828986511cef3c57ab9e479bb2e74419f
Hager Hilal <Hagerhilal2@gmail.com>
2021-04-10 07:51:27 +0200

    sales commissions

--- addons/custom_addons/accounting/sales-commission/models/product_template.py
+++ addons/custom_addons/accounting/sales-commission/models/product_template.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-   public_price = fields.Float(string="Public Price" , compute="compute_public_price")
+    public_price = fields.Float(string="Public Price" , compute="compute_public_price")
 
     def compute_public_price(self) :
         if self.pricelist_id : 
