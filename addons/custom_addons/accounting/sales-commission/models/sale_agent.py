7525a74d584de2aaaf9ab412cc5db493cbb52afd
Hager Hilal <Hagerhilal2@gmail.com>
2021-04-14 17:28:04 +0200

    commissions

--- addons/custom_addons/accounting/sales-commission/models/sale_agent.py
+++ addons/custom_addons/accounting/sales-commission/models/sale_agent.py
@@ -8,18 +8,18 @@ from datetime import datetime, timedelta
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
  
-    commissions = fields.Float(string="Commission" , readOnly = True , compute="compute_commissions" )
+    commissions = fields.Float(string="Commission" , readOnly = True )
     last_reset = fields.Datetime(string="Last Date" , readOnly = True , required = True , default=datetime.now())
 
     
     
-    def compute_commissions(self) : 
-        invoices = self.env['account.invoice'].search([('x_sale_agent.id' , '=' , self.id)])
-        com = 0 
-        for invoice in invoices : 
-            com += invoice.commission
-        self.commissions = com
-                        
+    #def compute_commissions(self) : 
+     #   invoices = self.env['account.invoice'].search([('x_sale_agent.id' , '=' , self.id)])
+      #  com = 0 
+       # for invoice in invoices : 
+        #    com += invoice.commission
+        #self.commissions = com
+                    
                 
 
 
