cbbd337b9a1f5485e5b2268e729579eb0048053c
enghagerhilal <hager@leen-eg.com>
2021-05-02 04:53:08 +0200

    modify

--- addons/custom_addons/others/sales_invoice_link/models/sale_order.py
+++ addons/custom_addons/others/sales_invoice_link/models/sale_order.py
@@ -9,10 +9,10 @@ from datetime import date , time
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    invoice_ids = fields.Many2many(comodel_name='account.invoice',
-                            relation='sale_invoice_link',
-                            column1='sale_order_id',
-                            column2='invoice_id')
+    #invoice_ids = fields.Many2many(comodel_name='account.invoice',
+    #                        relation='sale_invoice_link',
+    #                        column1='sale_order_id',
+    #                        column2='invoice_id')
     invoices = fields.One2many(comodel_name='account.invoice', inverse_name='sale_id' , String = 'Invoices')
 
                             
