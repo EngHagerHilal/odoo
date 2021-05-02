668689d1561604d602907704dd2df8f113e8bbba
enghagerhilal <hager@leen-eg.com>
2021-05-02 04:29:20 +0200

    modify

---
+++ addons/custom_addons/others/sales_invoice_link/models/account_invoice.py
@@ -0,0 +1,17 @@
+# See LICENSE file for full copyright and licensing details.
+from odoo import api ,fields, models
+from openerp.exceptions import ValidationError
+from datetime import datetime, timedelta
+from datetime import date , time
+
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    sale_ids = fields.Many2many(comodel_name='sale.order',
+                            relation='sale_invoice_link',
+                            column1='invoice_id',
+                            column2='sale_order_id')
+
+
---
+++ addons/custom_addons/others/sales_invoice_link/models/sale_order.py
@@ -0,0 +1,16 @@
+# See LICENSE file for full copyright and licensing details.
+from odoo import api ,fields, models
+from openerp.exceptions import ValidationError
+from datetime import datetime, timedelta
+from datetime import date , time
+
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    invoice_ids = fields.Many2many(comodel_name='account.invoice',
+                            relation='sale_invoice_link',
+                            column1='sale_order_id',
+                            column2='invoice_id')
+                            
