6c34687c0add834b1c8284e7c394932e9f6ae971
enghagerhilal <hager@leen-eg.com>
2021-05-02 05:19:30 +0200

    modify

--- addons/custom_addons/others/sales_invoice_link/models/__init__.py
+++ addons/custom_addons/others/sales_invoice_link/models/__init__.py
@@ -1,4 +1,4 @@
 # See LICENSE file for full copyright and licensing details.
 
 from . import account_invoice
-from . import sale_order
+#from . import sale_order
--- addons/custom_addons/others/sales_invoice_link/models/account_invoice.py
+++ addons/custom_addons/others/sales_invoice_link/models/account_invoice.py
@@ -9,10 +9,14 @@ from datetime import date , time
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    sale_ids = fields.Many2many(comodel_name='sale.order',
-                            relation='sale_invoice_link',
-                           column1='invoice_id',
-                           column2='sale_order_id')
-    
+    #sale_ids = fields.Many2many(comodel_name='sale.order',
+    #                        relation='sale_invoice_link',
+    #                      column1='invoice_id',
+    #                      column2='sale_order_id')
+    sale_id = fields.Many2one(
+        comodel_name='sale.order',
+        string='Sale Order',
+        readonly=True,
+    )
 
 
