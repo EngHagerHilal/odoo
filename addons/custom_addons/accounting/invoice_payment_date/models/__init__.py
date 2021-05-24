bfd3448632a3228bc6e5b48a192b0976ed32e73f
enghagerhilal <hager@leen-eg.com>
2021-05-24 11:04:03 +0200

    payment

--- addons/custom_addons/accounting/invoice_payment_date/models/__init__.py
+++ addons/custom_addons/accounting/invoice_payment_date/models/__init__.py
@@ -1,7 +1,3 @@
 # See LICENSE file for full copyright and licensing details.
 
-from . import sale_agent
 from . import account_invoice
-from . import product_template
-from . import product_category
-from . import stock_picking
